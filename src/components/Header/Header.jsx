cd31b856c435475a2cc78fabe20425882c950036
muhammad samim reza <muhammadsamimreza@gmail.com>
2025-10-20 23:02:16 +0600

    navbar done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ import Marquee from "react-fast-marquee";
 const Header = () => {
   return (
     <div className="w-11/12 mx-auto">
-      <header className="flex flex-col justify-center items-center py-5 space-y-1">
+      <header className="flex flex-col justify-center items-center pt-5 space-y-1">
         <img className="w-84" src={logo} alt="" />
         <p>Journalism Without Fear or Favour</p>
         <p>{format(new Date(), "EEEE, MMMM, dd, yyyy")} </p>
